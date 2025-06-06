004ffa9648045a64baa8edde33190c93323e4bb6
Giannis K <ikaragi23@yahoo.gr>
2025-06-06 17:37:46 +0300

    Remap neotree reveal

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,6 +1,5 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -11,15 +10,15 @@ return {
   },
   lazy = false,
   keys = {
-    { '<Tab>', ':Neotree toggle filesystem left<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader><Tab>', ':Neotree toggle filesystem left<CR>', desc = 'NeoTree reveal', silent = true },
     { '<leader><leader>', ':Neotree toggle buffers float<CR>', desc = 'Toggle buffers', silent = true },
     { '<leader>gt', ':Neotree toggle git_status float<CR>', desc = 'Toggle buffers', silent = true },
   },
-  -- opts = {
-  --   filesystem = {
-  --     follow_current_file = {
-  --       enabled = true,
-  --     },
-  --   },
-  -- },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
+    },
+  },
 }
